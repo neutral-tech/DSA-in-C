99b28b271a97e42d4af5622202b978edeaa3b427
neutral-tech <neutraltech.firoj@gmail.com>
2023-07-25 13:57:44 +0530

    Function to reverse the group of 'k' nodes

---
+++ LINKED_LIST/swapgroup.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct ListNode{
+    int val;
+    struct ListNode *next;
+};
+
+struct ListNode* swapgroup(struct ListNode* head,int k){
+    struct ListNode *temp,*list=head;
+    int i,j,group=0;
+    while(head!=NULL){
+        group++;
+        head=head->next;
+    }
+    group=group/k;
+    head=list;
+    int *arr=(int*)calloc(k,sizeof(int));
+    for(i=1;i<=group;i++){
+        temp=head;
+        for(j=0;j<k;j++){
+            *(arr+j)=head->val;
+            head=head->next;
+        }
+        head=temp;
+        for(j=k-1;j>=0;j--){
+            head->val=*(arr+j);
+            head=head->next;
+        }
+    }
+    return list;
+}
+
